627b2b3f16728181c1fae356ae5fa9865f7632b6
shchansky <gavrilenko71285@gmail.com>
2023-01-05 09:15:25 +0300

    commit

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -1,5 +1,12 @@
 import React from "react";
+import * as Units from "./units";
 
 export const Main = () => {
-  return <div>Main</div>;
+  return (
+    <div>
+      <h1>Main</h1>
+
+      <Units.Login />
+    </div>
+  );
 };
