a200bcb92d251a491b5aad710167e03ea208fff1
shchansky <gavrilenko71285@gmail.com>
2023-01-05 09:40:31 +0300

    commit

--- src/pages/main/units/login/login.tsx
+++ src/pages/main/units/login/login.tsx
@@ -1,5 +1,39 @@
 import React from "react";
 
 export const Login = () => {
-  return <div>Login</div>;
+  const [login, setLogin] = React.useState<string>("");
+  const [password, setPassword] = React.useState<string>("");
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    console.log("11111");
+    event.preventDefault();
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="login">Login:</label>
+          <input
+            name="login"
+            type="text"
+            value={login}
+            onChange={(ev) => setLogin(ev.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password:</label>
+          <input
+            name="password"
+            type="text"
+            value={password}
+            onChange={(ev) => setPassword(ev.target.value)}
+          />
+        </div>
+        <div>
+          <button>Sign In</button>
+        </div>
+      </form>
+    </div>
+  );
 };
